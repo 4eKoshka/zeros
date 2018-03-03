5d749a8c3c70e35327e37026fa9d9de56ee4ba42
4eKoshka <4ekoshka@gmail.com>
2018-03-02 18:58:30 -0800

    zero count perform

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number) {
+  var a;
+  var z=0;
+  var temp;
+   
+      
+  for(var a=1;a<=number; a++){
+	   if(a%5 === 0) {
+		   temp = a;
+		   while (temp % 5 == 0) {
+			   z++;
+			   temp = temp/5;
+		   }
+	   }
+  }
+  
+    
+  return z;
+  
   // your implementation
 }
